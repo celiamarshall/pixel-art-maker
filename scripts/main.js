e43beda5e0eba44435cf35e1d7058ecf963202fe
Celia Marshall <celia.m@gmail.com>
2018-10-03 16:46:28 -0700

    formatted current color

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+for (i = 0; i < 4980; i++){
+    newBox = document.createElement('div');
+    newBox.classList.add('box');
+    document.querySelector('.canvas').appendChild(newBox)
+}
+
+let color = 'black'
+
+
+
+function paintColor(event) {
+    color = event.target.dataset.color
+    console.log(color)
+    currentColor.innerHTML = `<div class="paint paint${color} style= background-color: ${color}"></div>`
+}
+
+const colorsArr = ['lightpink', 'hotpink', 'deeppink', 'darkred', 'red', 'orangered', 'orange', 'yellow', 'greenyellow', 'green', 'teal', 'blue', 'navy', 'indigo', 'purple']
+
+for (col of colorsArr) {
+    newPaint = document.createElement('div');
+    newPaint.classList.add('paint', 'paint' + col)
+    newPaint.style = `background-color: ${col}`
+    newPaint.dataset.color = col
+    document.querySelector('.palette').appendChild(newPaint)
+    const colorDiv = document.querySelector(`.paint${col}`)
+    colorDiv.addEventListener('click', paintColor)
+
+}
+
+currentText = document.createElement('div')
+currentText.innerHTML = "Current Color:"
+currentText.style = "font-size: 30px;" 
+currentText.style = "font-family: 'comic sans MS'"
+currentText.style = "margin-right: 10px; font-size: 30px; font-family: 'comic sans MS'"
+document.querySelector('.palette').appendChild(currentText)
+
+currentColor = document.createElement('div')
+document.querySelector('.palette').appendChild(currentColor)
+
+const boxes = document.querySelectorAll('.box')
+
+for (const box of boxes) {
+    box.addEventListener('click', (event) => {
+            event.target.classList.toggle(color)
+    })
+}
+
+
+
+
